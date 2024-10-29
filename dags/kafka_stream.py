e08a2057e1956ccade11a299e2b9de575e9af320
jonny0702 <jonathanvergararuiz@gmail.com>
2024-10-28 20:54:56 -0500

    :wrench: CONFIG: adding yml file to config docker images

---
+++ dags/kafka_stream.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+#Importing Appache Airflow
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner': 'air_flow_user',
+    'start_date': datetime(2024, 10, 27, 11, 00),
+}
+
+def gettin_data():
+    import requests
+
+    res = requests.get('https://randomuser.me/api/')
+    res = res.json()
+    res = res['results'][0]
+    return res
+
+def format_data(res):
+    data = {}
+
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name']=res['name']['last']
+    data['gender'] = res['gender']
+    data['address']= f"{str(location['street']['number'])}"
+
+    return data
+
+def streaming_data():
+    import json
+    data = gettin_data()
+    formated_data = format_data(data)
+
+    print(json.dumps(formated_data, indent= 3))
+
+
+
+
+#Creating a DAG
+
+# with DAG('user_automation',
+#         default_args = default_args,
+#         schedule_interval = '@daily',
+#         catchup = False) as dag:
+#
+#     streaming_task = PythonOperator(
+#         task_id = 'streaming_data_from_API',
+#         python_callable = streaming_data
+#     )
+
+streaming_data()
